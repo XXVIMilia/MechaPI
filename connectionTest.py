bca64eee3a35247989fab877227a2eb19ed770a5
Chauncey Deone Hester Jr <chauncey@debian.attlocal.net>
2021-10-09 22:28:25 -0500

    Connection test of bot successful

---
+++ connectionTest.py
@@ -0,0 +1,32 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+path = os.path.abspath(__file__)
+path = path[:-(len(__file__)+8)]
+
+
+load_dotenv(dotenv_path = path + "configFiles/.env")
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(f'{client.user} has connected to Discord!')
+    for member in guild.members:
+        print(member.name)
+
+@client.event
+async def on_message(message):
+    print(message.content)
+    
+
+
+
+client.run(TOKEN)
